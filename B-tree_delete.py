6f5e7114d9916c702121834a9431719e64cfd958
Growcompany <66565360+Growcompany@users.noreply.github.com>
2022-12-03 20:22:05 +0900

    Update B-tree_delete.py

--- B-tree_delete.py
+++ B-tree_delete.py
@@ -1,123 +1,179 @@
-def deleteBT(self, m, oldKey):
-    # oldKey가 있된 노드의 경로를 탐색하며, 스택에 경로 저장
-    found, stack = self.searchPath(self.T, m, oldKey, None)
-    if found == False:
-        return  # oldKey를 발견 못함. 삭제 불가.
+class getNode:
+    def __init__(self,m):
+        self.height = 1
+        self.n = 0
+        self.K = [0 for i in range(m+1)]
+        self.P = [None for i in range(m+1)]
 
-    x = stack.pop()
+class BT:
+    def __init__(self):
+        self.T = None
 
-    if oldKey is not in terminal node:  # oldKey를 내부 노드에서 발견.
-        internalNode = x
-        i = index of oldKey in internalNode
+    def insertBT(self, m, newKey):
 
-        # 후행키의 위치 탐색 (Ki와 일치하는 키는 찾지 못하나, 노드 경로는 검색)
-        found2, stack = self.searchPath(x.P[i], m, x.K[i], stack)
+        # 루트 노드 생성
+        if self.T == None:
+            self.T = getNode(m) # T.n ← 0; T. Ki ← null; Pi ← null;
+            self.T.K[1] = newKey
+            self.T.n = 1
+            return
 
-        # 후행키와 oldKey를 교환함.
-        x = stack.pop()  # x는 후행키가 있는 단말노드
-        temp = internalNode.K[i]
-        internalNode.K[i] = x.K[1]
-        x.K[1] = temp  # 이제 x.K[1]이 oldKey임
+        # newKey를 삽입할 노드의 경로를 탐색하며, 스택에 경로 저장
+        found, stack = self.searchPath(m, newKey, None)
+        if found == True: # newKey를 발견함. 삽입 불가.
+            print('i', newKey, ': The key already exists')
+            return
 
-    finished = False
-    self.deleteKey(self, m, x, oldKey) # 노드 x에서 oldKey를 삭제
+        # newKey가 없으므로, T에 삽입 (이제 x는 null)
+        finished = False
+        print("stack(len):",len(stack))
+        x = stack.pop()
+        print("now_x:",x.K,"now_x.n:",x.n)
+        left = None # 새로 분할된 왼쪽 노드를 담을 변수
+        y = None # 새로 분할된 오른쪽 노드를 담을 변수
 
-    if stack:
-        y = stack.pop() # 노드 y는 x의 부모 노드
-
-    while True:
-        if isRoot(x) or x.n >= (round(m/2)-1): # underflow 발생하지 않음
-            finished = True
-        else: # underflow 발생
-            # 키 재분배 또는 노드 합병을 위한 형제 노드를 결정
-            bestSibling = self.bestSibling(T, m, x, y)
-
-            if y.P[bestSibling].n > (round(m/2)- 1): # bestSibling에서 재분배
-                redistributeKeys(T, m, x, y, bestSibling)
+        while True:
+            if x.n < m-1: # Overflow 발생 여부 검사
+                # Overflow 발생 안함. newKey를 노드 x의 올바른 위치에 삽입
+                self.insertKey(m, x,left, y, newKey)
                 finished = True
-            else: # bestSibling과 노드 합병
-                mergeNode(T, m, x, y, bestSibling)
-                x = y
+            else: # Overflow 발생
+                # x를 newKey을 기준으로 분할, 분할된 노드 반환
+                left, newKey, y = self.splitNode(m, x, left, y, newKey)
+                print("111231x:", x.K, "1231123123x.n:", x.n)
+
                 if stack:
-                    y = stack.pop()
-                else:
+                    x = stack.pop()
+                else: # 트리의 레벨이 하나 증가
+                    self.T = getNode(m)
+                    self.T.K[1] = newKey
+                    self.T.n = 1
+                    self.T.P[0] = left
+                    self.T.P[1] = y
+                    print("*****************************************T.K:,T.n",self.T.K,self.T.n)
+                    print("T.P[0]:,T.P[0].n:", self.T.P[0].K,self.T.P[0].n)
+                    print("T.P[1]:,T.P[0].n:", self.T.P[1].K,self.T.P[1].n)
                     finished = True
-        if not finished:
-            continue
-        break
+            if not finished: continue
+            break
 
-    if y.n == 0: # y에 키가 없음.(비어 있음)
-        T = y.P[0]
-        discard y node # old root를 삭제, 트리 높이가 하나 줄어듬.
+    def searchPath(self, m, key, stack):
+        if stack == None:
+            stack = []
 
-    def deleteKey(self, m, x, oldKey):
-        # oldKey의 위치 i를 탐색
+        x = self.T
+
+        while True:
+            i = 1
+            while i <= x.n and key > x.K[i]:
+                i += 1
+
+            # 삽입할 키를 발견함. 삽입 불가
+            if i <= x.n and key == x.K[i]:
+                return True, stack
+
+            # for some i where K[i–1] < key < K[i], 삽입할 키를 아직 발견하지 못함.
+            stack.append(x)
+            x = x.P[i-1]
+            if x is not None: continue
+            break
+
+        return False, stack
+
+    def insertKey(self, m, x,left, y, newKey):
+        # newKey 보다 큰 키들을, 오른쪽으로 한 칸씩 이동
+        i = x.n
+        while i>=1 and newKey < x.K[i]:
+            x.K[i+1] = x.K[i]
+            x.P[i+1] = x.P[i]
+            i = i-1
+
+        while x.K[i] < newKey:
+            i += 1
+            if x.K[i] == 0: break
+
+        # newKey를 삽입
+        x.K[i] = newKey
+        x.P[i-1] = left
+        x.P[i] = y
+        x.n = x.n + 1
+
+
+    def splitNode(self, m, x,left, y, newKey):
+        tempNode = x # 오버플로우를 위한 임시 노드(x와 newKey를 저장)
+        print("전tempNode:", tempNode.K)
+        self.insertKey(m, tempNode,left, y, newKey)
+        print("tempNode:",tempNode.K)
+        centerKey = tempNode.K[round(m/2)] # 분할 기준인 중앙값
+
+        left = getNode(m) # centerKey 이전 값을 노드 x로 복사
         i = 1
-        while oldKey > x.K[i]:
-            i = i + 1
-
-        # oldKey 보다 큰 키들을 왼쪽으로 한 칸씩 이동
-        while i <= x.n:
-            x.K[i] = x.K[i+1]
-            x.P[i] = x.P[i+1]
-            i = i + 1
-        x.n = x.n - 1
-
-    def bestSibling(self, m, x, y):
-        # y에서 x의 위치 i를 탐색
-        i = 0
-        while y.P[i] != x:
+        while tempNode.K[i] < centerKey:
+            left.K[i] = tempNode.K[i]
+            left.P[i-1] = tempNode.P[i-1]
             i += 1
+            left.n += 1
+        left.P[i-1] = tempNode.P[i-1]
+        print("left:",left.K,"left.n:",left.n)
 
-        # 바로 인접한 두 형제 중, 키의 개수가 많은 형제를 bestSibling으로 선택함.
-        if I == 0:
-            bestSibling = i + 1 # 왼쪽 형제가 없음
-        elif i == y.n:
-            bestSibling = i – 1 # 오른쪽 형제가 없음
-        elif y.P[i–1].n >= y.P[i+1].n:
-            bestSibling = i – 1
-        else:
-            bestSibling = i + 1
-        return bestSibling
+        newNode = getNode(m) # centerKey 이후 값을 노드 newNode로 복사
 
-    def redistributeKeys(T, m, x, y, bestSibling):
-        # y에서 x의 위치 i를 탐색
-        i = 0
-        while y.P[i] != x:
+        i +=1
+        print("i",i,'tempNode.n:',tempNode.n)
+        while i <= tempNode.n:
+            newNode.K[i-m+1] = tempNode.K[i]
+            newNode.P[i-m] = tempNode.P[i-1]
             i += 1
+            newNode.n += 1
 
-        bestNode = y.P[bestSibling]
-        if bestSibling < i: # bestSibling이 왼쪽 형제 노드
-            lastKey = bestNode.K[bestNode.n]
-            self.insertKey(T, m, x, null, y.K[i])
-            self.deleteKey(T, m, bestNode, lastKey)
-            y.K[i] = lastKey
-        else: # bestSibling이 오른쪽 형제 노드
-            firstKey = bestNode.K[1]
-            self.insertKey(T, m, x, null, y.K[i + 1])
-            self.deleteKey(T, m, bestNode, firstKey)
-            y.K[i + 1] = firstKey
+        newNode.P[i-m] = tempNode.P[i-1]
 
-    def mergeNode(T, m, x, y, bestSibling):
-        i = 0 # y에서 x의 위치 i를 탐색
-        while y.P[i] != x:
-            i += 1
+        print("newNode:",newNode.K)
+        return left, centerKey, newNode
 
-        bestNode = y.P[bestSibling]
-        # 왼쪽 형제 노드로의 병합만 고려할 수 있도록 swap
-        if bestSibling > i:
-            bestSibling, i = i, bestSibling
-            bestNode, x = x, bestNode
-        # 왼쪽 형제 노드와 병합
-        bestNode.K[bestNode.n + 1] = y.K[i]
-        bestNode.n = bestNode.n + 1
-        j = 1
-        while j <= x.n:
-            bestNode.K[bestNode.n + 1] = x.K[j]
-            bestNode.P[bestNode.n] = x.P[j–1]
-            bestNode.n = bestNode.n + 1
+    def height(self,node): # 높이 구하기
+        if node is None:
+            return 0
+        left = self.height(node.left)
+        right = self.height(node.right)
 
-        bestNode.P[bestNode.n] = x.P[x.n]
-        self.deleteKey(m, y, y.K[i])
-        discard x node
+        return max(left, right) + 1
 
+    def inorderBT(self,node):
+        if node is not None:
+            print(node.K)
+            for x in node.P:
+                if x != 0:
+                    self.inorderBT(x)
+            #print(node.K)
+
+        #for x in node.P:
+        #    print(x.K)
+        #    self.inorderBT(x)
+        #    for i in x.K:
+        #        print("{0}".format(i), end=' ')
+
+f = open('BT-input.txt', 'r')
+
+BT_tree = BT()
+
+for line in f:
+    cmd, key = line.split()
+    if cmd == 'i':
+        BT_tree.insertBT(3,int(key))
+    elif cmd == 'd':
+        continue
+        # BT_tree.deleteBT(int(key))
+    BT_tree.inorderBT(BT_tree.T)
+    print()
+
+BT_tree_4 = BT()
+for line in f:
+    cmd, key = line.split()
+    if cmd == 'i':
+        BT_tree_4.insertBT(4,int(key))
+    elif cmd == 'd':
+        continue
+        # BT_tree.deleteBT(int(key))
+    BT_tree_4.inorderBT(BT_tree_4.T)
+    print()
